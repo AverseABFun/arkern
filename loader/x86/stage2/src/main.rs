b9d8aca75ce638c139a1b0438ad6c18f4e06fac0
Arthur Beck <averse.abfun@gmail.com>
2024-11-17 09:16:30 -0600

    it compiles and now is somewhat commented

--- loader/x86/stage2/src/main.rs
+++ loader/x86/stage2/src/main.rs
@@ -1,48 +1,78 @@
 #![no_std]
 #![no_main]
-#![allow(dead_code)]
+
 use core::{arch::asm, panic::PanicInfo, ptr::write_volatile};
 
 #[no_mangle]
-#[link_section = ".start"]
+#[link_section = ".start"] // probably a better way to do this, but oh well!
 pub unsafe extern "C" fn _start() -> ! {
-    letter_to_screen(b'A', VideoBufferColor::Red);
+    calculated_clear_screen(b'A', 0x1f);
+    // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
     halt()
 }
 
+#[allow(dead_code)]
+#[derive(Copy, Clone)]
 enum VideoBufferColor {
-    Black,
-    Blue,
-    Green,
-    Cyan,
-    Red,
-    Purple,
-    Brown,
-    Gray,
-    DarkGray,
-    LightBlue,
-    LightGreen,
-    LightCyan,
-    LightRed,
-    LightPurple,
-    Yellow,
-    White,
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Purple = 5,
+    Brown = 6,
+    Gray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    LightPurple = 13,
+    Yellow = 14,
+    White = 15,
 }
 
-#[inline(always)]
-unsafe fn letter_to_screen(letter: u8, color: VideoBufferColor) {
-    let mut ptr = 0xB8000 as *mut u8;
+unsafe fn letter_to_screen(offset: usize, letter: u8, text_color: u8, background_color: u8) {
+    let color: u8 = ((background_color as u8) << 4) | (text_color as u8);
+    calculated_letter_to_screen(offset, letter, color)
+}
+
+unsafe fn calculated_letter_to_screen(offset: usize, letter: u8, calculated_color: u8) {
+    let mut ptr = (0xB8000 + offset) as *mut u8;
     write_volatile(ptr, letter);
     ptr = ptr.wrapping_add(1);
-    write_volatile(ptr, color as u8);
+    write_volatile(ptr, calculated_color);
 }
 
+unsafe fn clear_screen(letter: u8, text_color: u8, background_color: u8) {
+    let mut offset: usize = 1;
+    loop {
+        if offset > 5000 {
+            break;
+        }
+        letter_to_screen(offset, letter, text_color, background_color);
+        offset += 3;
+    }
+}
+
+unsafe fn calculated_clear_screen(letter: u8, calculated_color: u8) {
+    let mut offset: usize = 0;
+    loop {
+        if offset > 5000 {
+            break;
+        }
+        calculated_letter_to_screen(offset, letter, calculated_color);
+        offset += 2;
+    }
+}
+
+#[inline(always)] // simple enough that we should always inline(in total literally two compiled instructions)
 fn halt() -> ! {
     unsafe { asm!("hlt") }
-    loop {}
+    loop {} // even though the previous instruction halts the CPU, rust doesn't know that and assumes that the function returns
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    halt()
+    halt() //TODO: log the panic if a debugger is connected
 }
