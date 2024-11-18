fb9ecf4b5117a47d14fb334d9d694ae5acaac8ec
Arthur Beck <averse.abfun@gmail.com>
2024-11-18 21:46:46 +0000

    stage2 stuff

--- stage2/x86/multiboot/src/main.rs
+++ stage2/x86/multiboot/src/main.rs
@@ -1,7 +1,25 @@
 #![no_std]
 #![no_main]
+#![feature(naked_functions)]
 
-use core::{arch::asm, panic::PanicInfo, ptr::write_volatile};
+use core::{
+    arch::{asm, naked_asm},
+    panic::PanicInfo,
+    ptr::write_volatile,
+};
+
+#[naked]
+#[no_mangle]
+#[link_section = ".multiboot"]
+pub unsafe extern "C" fn multiboot_header() {
+    naked_asm!(
+        ".align 4",
+        ".long 0x1BADB002", // Magic number
+        ".long 0x03",       // Flags (e.g., aligned modules)
+        ".long {checksum}", // Checksum (-(magic + flags))
+        checksum = const -(0x1BADB002i32 + 0b11i32),
+    );
+}
 
 #[no_mangle]
 #[link_section = ".start"] // probably a better way to do this, but oh well!
@@ -9,7 +27,7 @@ pub unsafe extern "C" fn _start() -> ! {
     clear_screen(b' ', VideoBufferColor::White, VideoBufferColor::Black); // clear the screen
     string_to_screen(
         0,
-        b"Arkern booting via BIOS...",
+        b"Arkern booting kernel...",
         VideoBufferColor::White,
         VideoBufferColor::Black,
     );
